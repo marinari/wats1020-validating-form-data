1267f697c4b6ace42f5b3a621ba102f008db76a5
Arianna Marin <marin.arianna@gmail.com>
2016-05-12 01:12:32 -0700

    added js ready handler, added error and success color, changed thank you page text

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+$(document).ready(function() {
+    $('.form-group').formValidation({
+        framework: 'bootstrap',
+        //adjusting feedback icons use glyphicon checkmark and cross mark
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh',
+        },
+
+    });
+});
